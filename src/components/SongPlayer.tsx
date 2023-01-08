92173ed767cd96f716e1f07dab33556d6a66e234
Genti Jashari <gentijashari5@gmail.com>
2023-01-08 20:14:03 +0100

    feat: spotify track embed iframe

--- src/components/SongPlayer.tsx
+++ src/components/SongPlayer.tsx
@@ -5,7 +5,7 @@ export default function SongPlayer() {
   const [renderPlayer, setRenderPlayer] = useState(false);
 
   const token =
-    'BQAtnbCw4-9_gk0CvSVNIuJGOrok2iY8Xj248PQ7xAO2xgsfzf1XKM7EzioNbjFglapocugWvJHRFGtHAzlp6ja2mngmHe7FmobHaYHlN0h0oah2dItvY-x4rjWMO9dbXHu0fZTret1noGvlYugP4nnZFpVmzRXYEw';
+    'BQDj7bkaN_RqiG1xMdaPTtS7Udt9W8yPBDYcmmADZrQEnqbSf-1BnF9uFZASyaR_vrJCT9W0DCbyoxs_B-PMBPdl-okBtrZptxSbJtJJLZJj513V_W4';
   const uris = ['spotify:track:5BvGdjGZKbsrtfA05Yg23W'];
 
   useEffect(() => {
@@ -15,6 +15,15 @@ export default function SongPlayer() {
   return (
     <div>
       <div>SongPlayer</div>
+      <iframe
+        style={{ borderRadius: '12px' }}
+        src='https://open.spotify.com/embed/track/0Tokm27V7U5xUMfxCQk8Nu?utm_source=generator'
+        width='100%'
+        height='352'
+        allowFullScreen={true}
+        allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+        loading='lazy'
+      ></iframe>
       {renderPlayer && (
         <SpotifyPlayer syncExternalDevice={false} token={token} uris={uris} />
       )}
