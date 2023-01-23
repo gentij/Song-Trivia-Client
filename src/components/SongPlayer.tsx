35a2924a8dad96524eba952828db6e1eb4b94c47
davud <davudgorqi@gmail.com>
2023-01-23 15:07:37 +0100

    feat(song-player): can fetch playlist tracks and play music controller from script

--- src/components/SongPlayer.tsx
+++ src/components/SongPlayer.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import axios from 'axios';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 
+const playlistId = '42HZjTRhG1eXWu69c2KsLG'; // temp
+
 export default function SongPlayer() {
-  const playlistId = '42HZjTRhG1eXWu69c2KsLG';
+  const [currentTrack, setCurrentTrack] = useState(0);
+  // this is so we can play audio using audioRef.current.play()
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const { data: playlistTracks, status } = useQuery({
     queryKey: 'playlist',
@@ -18,26 +22,47 @@ export default function SongPlayer() {
     return <p>Loading...</p>;
   }
 
-  console.log(playlistTracks);
+  const handleNextTrack = () => {
+    if (currentTrack < playlistTracks.length - 1) {
+      setCurrentTrack(currentTrack + 1);
+    }
+  };
 
-  // const trackUri = '03yOjwHoOPDlTUg0NRxN6t';
+  const handlePreviousTrack = () => {
+    if (currentTrack > 0) {
+      setCurrentTrack(currentTrack - 1);
+    }
+  };
 
-  // const spotifyFrame = useRef<any>(null);
-
-  // useEffect(() => {
-  //   console.log(
-  //     spotifyFrame?.current?.contentWindow.document.querySelector(
-  //       'data-testid="play-pause-button"'
-  //     )
-  //   );
-  // }, [spotifyFrame]);
-
-  return <></>;
+  return (
+    <div>
+      {playlistTracks.length > 0 && (
+        <div>
+          <audio
+            ref={audioRef}
+            src={playlistTracks[currentTrack].preview_url}
+            controls={true}
+          />
+          <button
+            className='rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700'
+            onClick={handlePreviousTrack}
+          >
+            Previous
+          </button>
+          <button
+            className='rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700'
+            onClick={handleNextTrack}
+          >
+            Next
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-async function getPlaylistTracks(playlistId: string) {
-  return axios
+const getPlaylistTracks = async (playlistId: string) =>
+  axios
     .get(`http://localhost:4000/playlists/${playlistId}`)
     .then((res) => res.data)
     .then((data) => data.data);
-}
