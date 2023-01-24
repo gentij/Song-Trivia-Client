9e0ea5c3c5f0dbf65a7ca88859727e4558ec09bd
Genti Jashari <gentijashari5@gmail.com>
2023-01-24 15:17:58 +0100

    feat: import font and add secondary color

--- src/components/GenreCard/GenreCard.tsx
+++ src/components/GenreCard/GenreCard.tsx
@@ -8,8 +8,8 @@ interface GenreCardProps {
 const GenreCard: React.FC<GenreCardProps> = ({ name, artists }) => {
   return (
     <div className='flex h-[100px] w-[200px] flex-col justify-between rounded-xl bg-amber-400 p-1'>
-      <h4 className='text-center text-xl text-primary-500'>{name}</h4>
-      <p className='text-secondary-500 text-sm'>
+      <h4 className='text-center text-xl font-bold text-primary-500'>{name}</h4>
+      <p className='text-sm font-semibold text-secondary-500'>
         {artists.map(
           (artist, index) =>
             `${artist}${index === artists.length - 1 ? ' and more' : ', '}`
